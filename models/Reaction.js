a463cdd2290ee257236f756a6e12eb6139f7534e
exonerated <jordansmithofficially@gmail.com>
2022-11-09 23:36:24 -0500

    fix

---
+++ models/Reaction.js
@@ -0,0 +1,7 @@
+const { Schema, Types } = require('mongoose');
+
+const thoughtSchema = new Schema(
+    {
+        // Refer to the assignment readme to see what data to fill out
+    }
+)
---
+++ models/Thought.js
@@ -0,0 +1,9 @@
+const { Schema, Types } = require('mongoose');
+
+const thoughtSchema = new Schema(
+    {
+        // Refer to the assignment readme to see what data to fill out
+    }
+)
+
+module.exports = thoughtSchema;
